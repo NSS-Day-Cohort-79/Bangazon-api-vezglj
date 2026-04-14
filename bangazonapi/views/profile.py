574e5436182c55c786d5e27d55ff01f231037076
Ethan Omessi <ethanomessi@gmail.com>
2026-04-14 11:08:40 -0500

    bug fixed

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -243,7 +243,7 @@ class Profile(ViewSet):
             """
 
             try:
-                open_order = Order.objects.get(customer=current_user)
+                open_order = Order.objects.get(customer=current_user, payment_type__isnull=True) #literally just added the filter payment_type__isnull=True
                 print(open_order)
             except Order.DoesNotExist as ex:
                 open_order = Order()
