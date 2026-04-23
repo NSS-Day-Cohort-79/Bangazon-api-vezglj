8670242bb1e1d3ff27a681378e4fa2df03c2b17f
Jordan Haddock <haddockjordan40@gmail.com>
2026-04-23 10:50:43 -0500

    new commit

--- bangazonapi/models/__init__.py
+++ bangazonapi/models/__init__.py
@@ -8,8 +8,5 @@ from .recommendation import Recommendation
 from .rating import Rating
 from .favorite import Favorite
 from .productrating import ProductRating
-<<<<<<< HEAD
 from .stores import Store
-=======
 from .productlike import ProductLike
->>>>>>> 4a607bc0412242ee629432ca7a292455344510d3
